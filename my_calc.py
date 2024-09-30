6d7bd95ca34a5ad0c270ccfb8bf61dc54ac4378b
ekkeeley <emma.keeley2@nhs.net>
2024-09-30 14:21:53 +0100

    FIX: Fix loop error in My_calc

--- my_calc.py
+++ my_calc.py
@@ -1,9 +1,14 @@
 def add_numbers(num_1, num_2):
-    result = num_1 + num_2
+    results = num_1 + num_2
 
     return results
 
-def subtract_numbers(num1, num_2):
-    results = num1 - num_2
+def subtract_numbers(num_1, num_2):
+    results = num_1 - num_2
 
+    return results
+
+def multiply_numbers(num_1, num_2):
+    results = num_1 * num_2
+    
     return results
