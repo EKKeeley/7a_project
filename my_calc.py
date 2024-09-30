e32e83da6c48becbb86b85481ae6567c66c92486
ekkeeley <emma.keeley2@nhs.net>
2024-09-30 11:57:20 +0100

    MY_CALC: add multiplyer

--- my_calc.py
+++ my_calc.py
@@ -3,7 +3,13 @@ def add_numbers(num_1, num_2):
 
     return results
 
-def subtract_numbers(num1, num_2):
-    results = num1 - num_2
+def subtract_numbers(num_1, num_2):
+    results = num_1 - num_2
 
+    return results
+
+def multiply_numbers(num_1, num_2):
+    while True:
+        results = num_1 * num_2
+    
     return results
